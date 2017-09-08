c5f725b4f3c64a0e3a8e935ad5db312d9c8012e3
aniket sharma <aniket965.as@gmail.com>
2017-09-08 16:44:57 +0530

    added engagement and disgust

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { LineChart, Line, XAxis, YAxis,Area,AreaChart, CartesianGrid, Tooltip, Legend } from 'recharts'
+import { XAxis, YAxis, Area, AreaChart, CartesianGrid, Tooltip } from 'recharts'
 var firebase = require('firebase');
 var config = {
   apiKey: "AIzaSyCOmubrc3gEd6LOW5UfRH5LVaL-GFgRCgk",
@@ -10,25 +10,19 @@ var config = {
   messagingSenderId: "481329884022"
 };
 firebase.initializeApp(config);
-const data = [
-  { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
-  { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
-  { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
-  { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
-  { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
-  { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
-  { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
-];
-
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { x: 0, data: [{  name:  "",
-        uv: 0}] };
+    this.state = {
+      x: 0, data: [{
+        name: "",
+        uv: 0
+      }]
+    };
     this.handlefirebase = this.handlefirebase.bind(this);
   }
   handlefirebase(val) {
-    this.setState( {data:val})
+    this.setState({ data: val })
   }
 
   componentWillMount() {
@@ -37,15 +31,13 @@ class App extends Component {
     ref.once('value', function (snapshot) {
 
       var data_copy = [];
-    var x  = 0; 
-    
-
+      var x = 0;
       snapshot.forEach(function (childSnapshot) {
         var childKey = childSnapshot.val();
-        
         data_copy.push({
-          name:x,
-          value:childKey.engagement
+          name: x,
+          engagement: childKey.engagement,
+          disgust: childKey.disgust
         })
         x++;
       });
@@ -57,25 +49,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <AreaChart width={730} height={250} data={this.state.data}
-      margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-      <defs>
-        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-          <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-          <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-        </linearGradient>
-        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-          <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-          <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
-        </linearGradient>
-      </defs>
-      <XAxis dataKey="name" />
-      <YAxis />
-      <CartesianGrid strokeDasharray="3 3" />
-      <Tooltip />
-      <Area type="monotone" dataKey="value" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-      {/* <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" /> */}
-    </AreaChart>
+        <h1 className="lol" style={{ marginLeft: '20vw', fontFamily: 'Encode Sans Condensed', fontSize: '5rem' }}>Course performance Level Graph</h1>
+        <AreaChart width={1650} height={650} data={this.state.data}
+          margin={{ top: 0, right: 30, left: 80 }}>
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area type="monotone" dataKey="engagement" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+          <Area type="monotone" dataKey="disgust" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+        </AreaChart>
       </div>
     );
   }
